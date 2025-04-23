ec60e8c4ac8bbc6c62a1629ee3f7ee3e3e869907
Cory Tamburrino <ctamburrino@sandiego.edu>
2025-04-22 21:25:24 -0700

    Updated run experiment to use factory methods

--- train/run_experiment.py
+++ train/run_experiment.py
@@ -1,10 +1,11 @@
 import sys
 import os
-
 # Add the project root directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from data.parse import parse_json, combine_text_fields
+from vectorizer_factory import get_vectorizer
+from model_factory import get_model
 from vectorizers.tfidf_vectorizer import TfidfVectorizerWrapper
 from models.mlp import MLPClassifier
 from sklearn.model_selection import train_test_split
@@ -13,12 +14,18 @@ import numpy as np
 
 file_path = "Movies_and_TV.json"
 
+# tfidf or tokenizer
+vectorizer_name = "tokenizer"
+# mlp or cnn
+model_name = "cnn"
+
 print("Parsing data...")
 data = parse_json(file_path,1000)
 texts, labels = combine_text_fields(data)
 
+# num_words and max_length are optional parameters for tokenizer vectorizer
 print("Vectorizing text...")
-vectorizer = TfidfVectorizerWrapper()
+vectorizer = get_vectorizer(vectorizer_name, num_words=10000, max_length=100)
 X = vectorizer.fit_transform(texts)
 
 print("Encoding labels...")
@@ -30,9 +37,9 @@ print("Splitting data...")
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 print("Training MLP...")
-mlp = MLPClassifier(input_dim=X.shape[1])
-mlp.train(X_train, y_train, X_test, y_test)
+model = get_model(model_name, X.shape[1])
+model.train(X_train, y_train, X_test, y_test)
 
 print("Evaluating MLP...")
-loss, accuracy = mlp.evaluate(X_test, y_test)
+loss, accuracy = model.evaluate(X_test, y_test)
 print(f"Test Loss: {loss:.4f}, Test Accuracy: {accuracy:.4f}")
