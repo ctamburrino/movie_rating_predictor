c688a12ea096ca4a9f64ce2354277080081ab518
Cory Tamburrino <ctamburrino@sandiego.edu>
2025-05-11 23:34:33 -0700

    Added parameters to tfidf constructor

--- vectorizers/tfidf_vectorizer.py
+++ vectorizers/tfidf_vectorizer.py
@@ -1,12 +1,11 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-
 class TfidfVectorizerWrapper:
     """
     A wrapper class for TfidfVectorizer to provide a consistent interface
     for vectorizing text, regardless of the vectorizer used.
     """
-    def __init__(self, max_features):
+    def __init__(self, max_features=10000):
         """
         Initializes the TF-IDF vectorizer.
 
@@ -16,7 +15,14 @@ class TfidfVectorizerWrapper:
         """
         self.vectorizer = TfidfVectorizer(
             max_features=max_features,
-            stop_words='english')
+            stop_words='english',
+            ngram_range=(1, 2),  # Use both unigrams and bigrams
+            min_df=2,  # Ignore terms that appear in less than 2 documents
+            max_df=0.95,  # Ignore terms that appear in more than 95% of documents
+            sublinear_tf=True,  # Apply sublinear scaling to term frequencies
+            analyzer='word',
+            token_pattern=r'(?u)\b\w\w+\b'  # Match words with 2 or more characters
+        )
 
     def fit_transform(self, texts):
         """
@@ -40,5 +46,4 @@ class TfidfVectorizerWrapper:
         Returns:
         - scipy.sparse.csr_matrix: The transformed TF-IDF matrix.
         """
-
         return self.vectorizer.transform(texts)
