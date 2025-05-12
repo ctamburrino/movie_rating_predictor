29df4d9ea662bdfbbcc88ef1a289539c622331b8
Cory Tamburrino <ctamburrino@sandiego.edu>
2025-05-11 23:34:47 -0700

    Added docstring to vectorizer_factory

--- train/vectorizer_factory.py
+++ train/vectorizer_factory.py
@@ -1,4 +1,14 @@
 def get_vectorizer(vectorizer_name, **kwargs):
+    """
+    Factory function to get a vectorizer based on the vectorizer name.
+
+    Parameters:
+        vectorizer_name (str): The name of the vectorizer to get.
+        **kwargs: Additional keyword arguments to pass to the vectorizer.
+
+    Returns:
+        A vectorizer object.
+    """
     if vectorizer_name == "tfidf":
         from vectorizers.tfidf_vectorizer import TfidfVectorizerWrapper
         return TfidfVectorizerWrapper(**kwargs)
